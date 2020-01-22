79bda00100cf9bafb9ba07d0d8aa2e3f73edd302
Michael Ziegler <michael.ziegler@proemion.com>
2020-01-22 08:42:04 +0100

    add test for remote close when the light barrier is blocked

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -76,6 +76,36 @@ void test_remote_close_uninterrupted() {
     TEST_ASSERT_TRUE(step.trigger);
 }
 
+void test_remote_close_light_barrier_blocked() {
+    StateMachine statemachine;
+
+    esp_state_t input = {
+        .sensor_gate_up    = SENSOR_ACTIVE,
+        .sensor_gate_down  = SENSOR_CLEAR,
+        .sensor_lb_blocked = SENSOR_ACTIVE,
+        .sensor_lb_clear   = SENSOR_CLEAR,
+        .millis = 100
+    };
+    step_t step = statemachine.step(&input);
+    TEST_ASSERT_EQUAL(GATE_OPEN, step.current_state);
+
+    cmd_result_t res = statemachine.cmd_close(500);
+    TEST_ASSERT_EQUAL(COMMAND_ACCEPTED, res);
+
+    input.millis = 800;
+    step = statemachine.step(&input);
+    TEST_ASSERT_EQUAL(GATE_OPEN, step.current_state);
+    TEST_ASSERT_FALSE(step.trigger);
+
+    res = statemachine.cmd_commit(10536);
+    TEST_ASSERT_EQUAL(COMMAND_ACCEPTED, res);
+
+    input.millis = 1600;
+    step = statemachine.step(&input);
+    TEST_ASSERT_EQUAL(GATE_OPEN, step.current_state);
+    TEST_ASSERT_FALSE(step.trigger);
+}
+
 int main( int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_init_open);
