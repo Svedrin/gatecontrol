4ee0fef7349252bdfc9c25b0a1df1928d73da9ce
Michael Ziegler <michael.ziegler@proemion.com>
2020-01-22 10:14:08 +0100

    test the edge cases

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -146,6 +146,37 @@ void test_monitoredsensor() {
     sensor.set_pin_states(1, 1);
     TEST_ASSERT_EQUAL(SENSOR_ERROR, sensor.read(240));
 
+    // Check the relay case: Active=LOW, Monitor=HIGH because
+    // monitor is connected to the idle-closed relay port
+    // Go back to active state, check that short errors are debounced correctly
+    sensor.set_pin_states(0, 1);
+    TEST_ASSERT_EQUAL(SENSOR_ACTIVE, sensor.read(260));
+
+    // Error, should be debounced until millis=330
+    // The relay is in the process of switching (and for some reason, that takes 40ms)
+    sensor.set_pin_states(1, 1);
+    TEST_ASSERT_EQUAL(SENSOR_ACTIVE, sensor.read(280));
+
+    sensor.set_pin_states(1, 1);
+    TEST_ASSERT_EQUAL(SENSOR_ACTIVE, sensor.read(300));
+
+    // Resolve error: The relay has now reached its target position
+    sensor.set_pin_states(1, 0);
+    TEST_ASSERT_EQUAL(SENSOR_CLEAR, sensor.read(320));
+
+    sensor.set_pin_states(1, 0);
+    TEST_ASSERT_EQUAL(SENSOR_CLEAR, sensor.read(340));
+
+    // The relay is in the process of switching back
+    sensor.set_pin_states(1, 1);
+    TEST_ASSERT_EQUAL(SENSOR_CLEAR, sensor.read(360));
+
+    sensor.set_pin_states(1, 1);
+    TEST_ASSERT_EQUAL(SENSOR_CLEAR, sensor.read(380));
+
+    // Resolve error: The relay has now reached its target position
+    sensor.set_pin_states(0, 1);
+    TEST_ASSERT_EQUAL(SENSOR_ACTIVE, sensor.read(400));
 }
 
 int main( int argc, char **argv) {
